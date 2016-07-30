b17d6f3dd1c6d6cf7cd816da6fb3fa2ddb5342cc
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2016-07-30 14:02:34 +0500

    Simple implementation for tvp parameters was added

---
+++ src/Tests/TipsAndTricksLibrary.Tests/DbCommands/TvpParameterTests.cs
@@ -0,0 +1,92 @@
+﻿namespace TipsAndTricksLibrary.Tests.DbCommands
+{
+    using System.Data.SqlClient;
+    using System.Linq;
+    using Dapper;
+    using TipsAndTricksLibrary.DbCommands;
+    using Xunit;
+
+    public class TvpParameterTests
+    {
+        private class TestEntity
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public int? Value { get; set; }
+
+            protected bool Equals(TestEntity other)
+            {
+                return Id == other.Id && string.Equals(Name, other.Name) && Value == other.Value;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((TestEntity) obj);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    var hashCode = Id;
+                    hashCode = (hashCode*397) ^ (Name != null ? Name.GetHashCode() : 0);
+                    hashCode = (hashCode*397) ^ Value.GetHashCode();
+                    return hashCode;
+                }
+            }
+        }
+
+        private static SqlConnection GetConnection()
+        {
+            var connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=tempdb;Integrated Security=True");
+            connection.Open();
+            return connection;
+        }
+
+        [Fact]
+        public void ShouldUseTvpInQuery()
+        {
+            // Given
+            var expected = new[]
+                           {
+                               new TestEntity {Id = 1, Name = "First", Value = 1},
+                               new TestEntity {Id = 2, Value = 2},
+                               new TestEntity {Id = 2, Name = "Third"}
+                           };
+            // When
+            TestEntity[] actual;
+            using (var connection = GetConnection())
+            {
+                connection.Execute(@"
+if type_id (N'[dbo].[TestList]') is null
+	create type [dbo].[TestList] as table(
+	    [Id] [int] not null,
+        [Name] [nvarchar](max) null,
+        [Value] [int] null)");
+
+                actual = connection.Query<TestEntity>(@"
+select * from @Param",
+                    new
+                    {
+                        Param = TvpParameter.Create("TestList",
+                            expected.Select(x =>
+                                new
+                                {
+                                    x.Id,
+                                    x.Name,
+                                    x.Value
+                                }))
+                    })
+                    .ToArray();
+            }
+
+            // Then
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/TipsAndTricksLibrary/DbCommands/TvpParameter.cs
@@ -0,0 +1,63 @@
+﻿namespace TipsAndTricksLibrary.DbCommands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using System.Reflection;
+    using Dapper;
+    using Microsoft.SqlServer.Server;
+
+    public static class TvpParameter
+    {
+        public static TvpParameter<TSource> Create<TSource>(string typeName, IEnumerable<TSource> source) where TSource : class
+        {
+            return new TvpParameter<TSource>(typeName, source);
+        }
+    }
+
+    public class TvpParameter<TSource> : SqlMapper.ICustomQueryParameter where TSource : class
+    {
+        private readonly string _typeName;
+        private readonly IEnumerable<TSource> _source;
+
+        public TvpParameter(string typeName, IEnumerable<TSource> source)
+        {
+            if(string.IsNullOrWhiteSpace(typeName))
+                throw new ArgumentNullException(nameof(typeName));
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            _typeName = typeName;
+            _source = source;
+        }
+
+        public void AddParameter(IDbCommand command, string name)
+        {
+            var sqlCommand = (SqlCommand)command;
+            var parameter = sqlCommand.Parameters.Add(name, SqlDbType.Structured);
+            parameter.Direction = ParameterDirection.Input;
+            parameter.TypeName = _typeName;
+
+            var parameterValue = new List<SqlDataRecord>();
+            var itemProperties = typeof(TSource).GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty);
+            var tvpDefinition = itemProperties.Select(x => SqlTypesMapper.CreateMetaData(x.Name, x.PropertyType)).ToArray();
+            foreach (var item in _source)
+            {
+                var record = new SqlDataRecord(tvpDefinition);
+                for (var i = 0; i < itemProperties.Length; i++)
+                {
+                    var propertyValue = itemProperties[i].GetValue(item);
+                    if (propertyValue == null)
+                        record.SetDBNull(i);
+                    else
+                        record.SetValue(i, propertyValue);
+                }
+                parameterValue.Add(record);
+            }
+
+            parameter.Value = parameterValue;
+        }
+    }
+}
