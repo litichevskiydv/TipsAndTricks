bc98b513b640d12e250f242de750afbb04a22aad
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2016-08-04 22:46:06 +0500

    Lower bound extensions were added

--- src/Tests/TipsAndTricksLibrary.Tests/Extensions/ArrayExtensionsTests.cs
+++ src/Tests/TipsAndTricksLibrary.Tests/Extensions/ArrayExtensionsTests.cs
@@ -9,6 +9,8 @@
     {
         [UsedImplicitly]
         public static IEnumerable<object[]> UpperBoundTestsData;
+        [UsedImplicitly]
+        public static IEnumerable<object[]> LowerBoundTestsData;
 
         static ArrayExtensionsTests()
         {
@@ -20,6 +22,16 @@
                                       new object[] {new[] {1, 2, 3}, 2, null, 1, null},
                                       new object[] {new[] {1, 2, 3}, 0, 1, null, 1},
                                   };
+            LowerBoundTestsData = new[]
+                                  {
+                                      new object[] {new[] {1, 2, 3}, 2, null, null, 1},
+                                      new object[] {new[] {1, 2, 3}, 0, null, null, 0},
+                                      new object[] {new[] {1, 2, 5}, 5, null, null, 2},
+                                      new object[] {new[] {1, 2, 5}, 6, null, null, null},
+                                      new object[] {new[] {1, 2, 3}, 2, null, 1, 1},
+                                      new object[] {new[] {1, 2, 3}, 3, null, 1, null},
+                                      new object[] {new[] {1, 2, 3}, 0, 1, null, 1},
+                                  };
         }
 
         [Theory]
@@ -27,6 +39,13 @@
         public void ShouldFindUpperBound(int[] source, int value, int? startIndex, int? endIndex, int? expectedResult)
         {
             Assert.Equal(expectedResult, source.UpperBound(value, startIndex: startIndex, endIndex: endIndex));
+        }
+
+        [Theory]
+        [MemberData(nameof(LowerBoundTestsData))]
+        public void ShouldFindLowerBound(int[] source, int value, int? startIndex, int? endIndex, int? expectedResult)
+        {
+            Assert.Equal(expectedResult, source.LowerBound(value, startIndex: startIndex, endIndex: endIndex));
         }
     }
 }
--- src/TipsAndTricksLibrary/Extensions/ArrayExtensions.cs
+++ src/TipsAndTricksLibrary/Extensions/ArrayExtensions.cs
@@ -50,5 +50,51 @@
 
             return UpperBound(source, value, Comparer<TSource>.Create(comparison), startIndex, endIndex);
         }
+
+        public static int? LowerBound<TSource>(this IReadOnlyList<TSource> source, TSource value, IComparer<TSource> itemsComparer = null,
+            int? startIndex = null, int? endIndex = null)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (source.Count == 0)
+                throw new ArgumentException(nameof(source));
+
+            var first = startIndex ?? 0;
+            if (first < 0 || first >= source.Count)
+                throw new ArgumentException(nameof(startIndex));
+
+            var last = (endIndex ?? source.Count - 1) + 1;
+            if (first > last || last > source.Count)
+                throw new ArgumentException(nameof(endIndex));
+
+            var comparer = itemsComparer ?? Comparer<TSource>.Default;
+
+            var count = last - first;
+            while (count > 0)
+            {
+                var current = first;
+                var step = count / 2;
+                current += step;
+
+                if (comparer.Compare(value, source[current]) > 0)
+                {
+                    first = current + 1;
+                    count -= step + 1;
+                }
+                else
+                    count = step;
+            }
+
+            return first == last ? (int?)null : first;
+        }
+
+        public static int? LowerBound<TSource>(this IReadOnlyList<TSource> source, TSource value, Comparison<TSource> comparison,
+            int? startIndex = null, int? endIndex = null)
+        {
+            if (comparison == null)
+                throw new ArgumentNullException(nameof(comparison));
+
+            return LowerBound(source, value, Comparer<TSource>.Create(comparison), startIndex, endIndex);
+        }
     }
 }
