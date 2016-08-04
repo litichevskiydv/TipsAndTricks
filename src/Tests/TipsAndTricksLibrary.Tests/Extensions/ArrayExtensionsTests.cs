7ac7615f500820b5704d69d3197cda1a96eeb9cc
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2016-08-04 22:19:59 +0500

    Upper bound simple implementation for arrays was added

---
+++ src/Tests/TipsAndTricksLibrary.Tests/Extensions/ArrayExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿namespace TipsAndTricksLibrary.Tests.Extensions
+{
+    using System.Collections.Generic;
+    using JetBrains.Annotations;
+    using TipsAndTricksLibrary.Extensions;
+    using Xunit;
+
+    public class ArrayExtensionsTests
+    {
+        [UsedImplicitly]
+        public static IEnumerable<object[]> UpperBoundTestsData;
+
+        static ArrayExtensionsTests()
+        {
+            UpperBoundTestsData = new[]
+                                  {
+                                      new object[] {new[] {1, 2, 3}, 2, null, null, 2},
+                                      new object[] {new[] {1, 2, 3}, 0, null, null, 0},
+                                      new object[] {new[] {1, 2, 5}, 5, null, null, null},
+                                      new object[] {new[] {1, 2, 3}, 2, null, 1, null},
+                                      new object[] {new[] {1, 2, 3}, 0, 1, null, 1},
+                                  };
+        }
+
+        [Theory]
+        [MemberData(nameof(UpperBoundTestsData))]
+        public void ShouldFindUpperBound(int[] source, int value, int? startIndex, int? endIndex, int? expectedResult)
+        {
+            Assert.Equal(expectedResult, source.UpperBound(value, startIndex, endIndex));
+        }
+    }
+}
---
+++ src/TipsAndTricksLibrary/Extensions/ArrayExtensions.cs
@@ -0,0 +1,45 @@
+﻿namespace TipsAndTricksLibrary.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class ArrayExtensions
+    {
+        public static int? UpperBound<TSource>(this IReadOnlyList<TSource> source, TSource value, int? startIndex = null, int? endIndex = null,
+            IComparer<TSource> itemsComparer = null)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (source.Count == 0)
+                throw new ArgumentException(nameof(source));
+
+            var first = startIndex ?? 0;
+            if (first < 0 || first >= source.Count)
+                throw new ArgumentException(nameof(startIndex));
+
+            var last = (endIndex ?? source.Count - 1) + 1;
+            if (first > last || last > source.Count)
+                throw new ArgumentException(nameof(endIndex));
+
+            var comparer = itemsComparer ?? Comparer<TSource>.Default;
+
+            var count = last - first;
+            while (count > 0)
+            {
+                var current = first;
+                var step = count / 2;
+                current += step;
+
+                if (comparer.Compare(value, source[current]) >= 0)
+                {
+                    first = current + 1;
+                    count -= step + 1;
+                }
+                else
+                    count = step;
+            }
+
+            return first == last ? (int?)null : first;
+        }
+    }
+}
