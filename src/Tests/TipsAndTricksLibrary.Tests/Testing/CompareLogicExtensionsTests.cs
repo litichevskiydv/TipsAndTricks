e2be066cfe979873794c89b3a1410221d0baee1f
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2016-08-06 00:42:37 +0500

    Simple extensions for compare logic were added

---
+++ src/Tests/TipsAndTricksLibrary.Tests/Testing/CompareLogicExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿namespace TipsAndTricksLibrary.Tests.Testing
+{
+    using JetBrains.Annotations;
+    using KellermanSoftware.CompareNetObjects;
+    using TipsAndTricksLibrary.Testing;
+    using Xunit;
+
+    public class CompareLogicExtensionsTests
+    {
+        private class TestEntity
+        {
+            public int Id { [UsedImplicitly] get; set; }
+
+            public string Name { get; set; }
+
+            public int Value { [UsedImplicitly] get; set; }
+        }
+
+        [Fact]
+        public void ShouldExcludePropretyFromComparsion()
+        {
+            // Given
+            var a = new TestEntity {Id = 1, Name = "a", Value = 1};
+            var b = new TestEntity { Id = 1, Name = "b", Value = 1 };
+
+            // When
+            var compareLogic = new CompareLogic()
+                .ExcludePropertyFromComparison<TestEntity, string>(x => x.Name);
+
+            // Then
+            Assert.True(compareLogic.Compare(a, b).AreEqual);
+        }
+    }
+}
---
+++ src/TipsAndTricksLibrary/Testing/CompareLogicExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace TipsAndTricksLibrary.Testing
+{
+    using System;
+    using System.Linq.Expressions;
+    using KellermanSoftware.CompareNetObjects;
+
+    public static class CompareLogicExtensions
+    {
+        public static CompareLogic ExcludePropertyFromComparison<TEntity, TProperty>
+            (this CompareLogic compareLogic, Expression<Func<TEntity, TProperty>> ignoredMember)
+        {
+            var nameIgnoredField = ((MemberExpression)ignoredMember.Body).Member.Name;
+            compareLogic.Config.MembersToIgnore.Add(nameIgnoredField);
+            return compareLogic;
+        }
+
+        public static CompareLogic SetMaxDifferencesCount(this CompareLogic compareLogic, int maxDifferencesCount)
+        {
+            compareLogic.Config.MaxDifferences = maxDifferencesCount;
+            return compareLogic;
+        }
+    }
+}
