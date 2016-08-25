67f6990ad0bcd81f5129a4c874f8c622a40b521b
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2016-08-26 00:29:38 +0500

    Simple nlog usage example was added

---
+++ src/Tests/TipsAndTricksLibrary.Tests/Logging/NlogUsageTests.cs
@@ -0,0 +1,24 @@
+﻿namespace TipsAndTricksLibrary.Tests.Logging
+{
+    using Microsoft.Extensions.Logging;
+    using NLog.Extensions.Logging;
+    using Xunit;
+
+    public class NlogUsageTests
+    {
+        private readonly ILogger _logger;
+
+        public NlogUsageTests()
+        {
+            var loggerFactory = new LoggerFactory()
+                .AddNLog();
+            _logger = loggerFactory.CreateLogger<NlogUsageTests>();
+        }
+
+        [Fact]
+        public void ShouldWriteInfoMessage()
+        {
+            _logger.LogInformation("Information was decoded successfully");
+        }
+    }
+}
