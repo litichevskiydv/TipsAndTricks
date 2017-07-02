8489f29728c1fa68d5d7822bf8983bc29a7d1b98
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-07-02 16:43:16 +0500

    Universal extension for obtaining conversions in type was added

---
+++ src/TipsAndTricksLibrary/Extensions/TypeExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace TipsAndTricksLibrary.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+
+    public static class TypeExtensions
+    {
+        public static Dictionary<Type, Func<object, TTo>> GetAllConversionsIn<TTo>()
+        {
+            var destinationType = typeof(TTo);
+            var availableConversions = new Dictionary<Type, Func<object, TTo>>();
+
+            var conversionsDefinitions = destinationType.GetMethods(BindingFlags.Public | BindingFlags.Static)
+                .Where(x => x.IsSpecialName && x.ReturnType == destinationType && x.GetParameters().Length == 1);
+
+            var objectType = typeof(object);
+            foreach (var definition in conversionsDefinitions)
+            {
+                var convertibleType = definition.GetParameters().Single().ParameterType;
+
+                var parameter = Expression.Parameter(objectType, "x");
+                var converter = Expression.Lambda(
+                        Expression.Convert(
+                            Expression.Convert(parameter, convertibleType),
+                            destinationType,
+                            definition),
+                        parameter)
+                    .Compile();
+
+                availableConversions.Add(convertibleType, (Func<object, TTo>)converter);
+            }
+
+            return availableConversions;
+        }
+    }
+}
--- src/TipsAndTricksLibrary/Redis/Converters/ValuesConverter.cs
+++ src/TipsAndTricksLibrary/Redis/Converters/ValuesConverter.cs
@@ -2,49 +2,23 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Linq.Expressions;
-    using System.Reflection;
     using Newtonsoft.Json;
     using StackExchange.Redis;
+    using TypeExtensions = Extensions.TypeExtensions;
 
     public class ValuesConverter : IValuesConverter
     {
-        private readonly Dictionary<Type, Func<object, RedisValue>> _availableConverters;
+        private readonly Dictionary<Type, Func<object, RedisValue>> _availableConversions;
 
         public ValuesConverter()
         {
-            var redisValueType = typeof(RedisValue);
-
-            Expression<Func<int, RedisValue>> intConverter = x => x;
-            var methodName = ((UnaryExpression) intConverter.Body).Method.Name;
-            var otherConverters = typeof(RedisValue).GetMethods(BindingFlags.Public | BindingFlags.Static)
-                .Where(x => x.Name == methodName && x.ReturnType == redisValueType)
-                .ToArray();
-
-            var objectType = typeof(object);
-            _availableConverters = new Dictionary<Type, Func<object, RedisValue>>();
-            foreach (var otherConverter in otherConverters)
-            {
-                var convertibleType = otherConverter.GetParameters().Single().ParameterType;
-
-                var parameter = Expression.Parameter(objectType, "x");
-                var converter = Expression.Lambda(
-                        Expression.Convert(
-                            Expression.Convert(parameter, convertibleType),
-                            redisValueType,
-                            otherConverter),
-                        parameter)
-                    .Compile();
-
-                _availableConverters.Add(convertibleType, (Func<object, RedisValue>) converter);
-            }
+            _availableConversions = TypeExtensions.GetAllConversionsIn<RedisValue>();
         }
 
         public RedisValue Convert<TValue>(TValue value)
         {
             Func<object, RedisValue> converter;
-            if (_availableConverters.TryGetValue(typeof(TValue), out converter))
+            if (_availableConversions.TryGetValue(typeof(TValue), out converter))
                 return converter(value);
             return JsonConvert.SerializeObject(value);
         }
