96932bf0ab7ae6a8a274422e0d57761ccd4616fc
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2018-05-29 08:00:59 +0500

    AppVeyor build was configured for Windows and Linux

--- test/TipsAndTricksLibrary.Tests/DatabaseTestsFixture.cs
+++ test/TipsAndTricksLibrary.Tests/DatabaseTestsFixture.cs
@@ -7,26 +7,40 @@
     [UsedImplicitly]
     public abstract class UsingDbTestBase
     {
-        private static readonly bool IsAppVeyor = Environment.GetEnvironmentVariable("Appveyor")?.ToUpperInvariant() == "TRUE";
-        private static readonly bool IsTravis = Environment.GetEnvironmentVariable("TRAVIS")?.ToUpperInvariant() == "TRUE";
+        private readonly bool _isAppVeyorWindows;
+        private readonly bool _isAppVeyorLinux;
 
-        protected static string ConnectionString
+        private readonly bool _isTravis;
+
+        private string ConnectionString
         {
             get
             {
-                if (IsAppVeyor)
+                if (_isAppVeyorWindows)
                     return @"Data Source = (local)\SQL2016;Initial Catalog=tempdb;User Id=sa;Password=Password12!";
-                return IsTravis
-                    ? @"Data Source = localhost;Initial Catalog=tempdb;User Id=sa;Password=Password12!"
+                if (_isAppVeyorLinux)
+                    return "Data Source = localhost;Initial Catalog=tempdb;User Id=sa;Password=Password12!";
+
+                return _isTravis
+                    ? "Data Source = localhost;Initial Catalog=tempdb;User Id=sa;Password=Password12!"
                     : @"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = tempdb; Integrated Security = True";
             }
         }
 
-        protected static SqlConnection GetConnection()
+        protected SqlConnection GetConnection()
         {
             var connection = new SqlConnection(ConnectionString);
             connection.Open();
             return connection;
+        }
+
+        protected UsingDbTestBase()
+        {
+            var isAppVeyor = Environment.GetEnvironmentVariable("APPVEYOR")?.ToUpperInvariant() == "TRUE";
+            _isAppVeyorWindows = isAppVeyor && Environment.GetEnvironmentVariable("CI_WINDOWS")?.ToUpperInvariant() == "TRUE";
+            _isAppVeyorLinux = isAppVeyor && Environment.GetEnvironmentVariable("CI_LINUX")?.ToUpperInvariant() == "TRUE";
+
+            _isTravis = Environment.GetEnvironmentVariable("TRAVIS")?.ToUpperInvariant() == "TRUE";
         }
     }
 }
